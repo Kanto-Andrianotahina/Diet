424dade7f982626eba91976afda7e3d997975b58
ahava <minoraherinirina72@gmail.com>
2023-07-10 14:38:39 +0200

    donnees user

--- schema.sql
+++ schema.sql
@@ -7,6 +7,8 @@ CREATE table gender (
     gender          VARCHAR(30)
 );
 
+INSERT into gender (gender) VALUES ('Homme'),('Femme');
+
 create table user (
     id              int auto_increment PRIMARY key,
     firstname       VARCHAR(30),
@@ -18,5 +20,14 @@ create table user (
     weigth          DECIMAL(3,3),
     password        VARCHAR(30)
 );
+INSERT INTO user (firstname, name, birthday, mail, id_gender, size, weight, password)
+VALUES ('John', 'Doe', '1990-05-15', 'john.doe@example.com', 1, 180.5, 75.2, 'password123'),
+        ('Andrianotahina', 'Kanto', '2003-08-21', 'kanto@gmail.com', 1, 170.5, 67, '123'),
+        ('Razafimiandrisoa', 'Tendry', '2003-04-04', 'tendry@gmail.com', 1, 165.5, 50, '456'),
+        ('Razafimiandrisoa', 'Tendry', '2003-04-04', 'tendry@gmail.com', 1, 165.5, 50, '456'),
+        ('Raherinirina', 'Mino', '2002-04-09', 'mino@gmail.com', 2, 160, 47, '789'),
+        ('Jane', 'Smith', '1985-09-22', 'jane.smith@example.com', 2, 165.8, 60.7, 'pass456');
+
+
 
 
