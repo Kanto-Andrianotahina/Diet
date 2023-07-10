7455d574c281ef8362cb09280c4120288bdd17a8
ManoarySarobidy <94636671+Tendrynyavo@users.noreply.github.com>
2023-07-10 13:55:16 +0300

    first commit

--- application/config/database.php
+++ application/config/database.php
@@ -76,10 +76,10 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
-	'username' => 'postgres',
+	'username' => 'root',
 	'password' => 'root',
-	'database' => 'diet',
-	'dbdriver' => 'postgre',
+	'database' => 'regime',
+	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
