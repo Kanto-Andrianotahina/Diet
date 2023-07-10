3639c675b94b3f7922d779ca8bc632c36cb9d258
ManoarySarobidy <94636671+Tendrynyavo@users.noreply.github.com>
2023-07-10 17:19:03 +0300

    insert

--- application/controllers/UserController.php
+++ application/controllers/UserController.php
@@ -1,10 +1,29 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
     
-    class UserController extends CI_Controller{
+    class UserController extends CI_Controller {
 
-        public function index(){
-            $this->load->view('FO/login-User');
+        public function login() {
+        
         }
+        
+        public function sign_up() {
+            try {
+                $this->load->model('User');
+                $name = "Tendry";
+                $first_name = "Ny Avo";
+                $birthday = "2023-09-01";
+                $email = "tendry@gmail.com";
+                $id_gender = 3;
+                $size = 120;
+                $weigth = 40;
+                $password = "Tendry";
+                $user = $this->User->getInstance($name, $first_name, $birthday, $email, $id_gender, $size, $weigth, $password);
+                $user->insert();
+            } catch (\Exception $e) {
+                echo $e;
+            }
+        }
+
     }
 ?>
